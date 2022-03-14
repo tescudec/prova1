7b4a6d24de5eedffa5216c49647a3bc6d205e0bd
tescudec <101245603+tescudec@users.noreply.github.com>
2022-03-14 12:29:39 +0100

    afegit canvi de velocitat , mida, angle i collision shape

--- Godot.gd
+++ Godot.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends Area2D
 
 
 # Declare member variables here. Examples:
@@ -8,7 +8,7 @@ export var velocitat : int = 20
 export var direccio = Vector2 (0,0)
 export var mida = Vector2 (1,1)
 export var posicio = Vector2 (500,500) 
-export  var rotacio = 90
+export  var rotacio = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Replace with function body.
@@ -17,7 +17,8 @@ func _ready():
 	scale = mida
 # Called every frame. 'delta' is the elapsed time since the previous frame
 func _process(delta):
-	position += velocitat * delta *direccio 
+	var vel = int(20)
+	
 #	if position.x >= 1024:
 #		velocitat.x = -velocitat.x
 #	if position.x <= 0:
@@ -27,4 +28,30 @@ func _process(delta):
 #	if position.y <= 0:
 #		velocitat.y = -velocitat.y
 	if Input.is_action_pressed("mou dreta"):
-		direccio = Vector2(1,0)
+		direccio = Vector2.RIGHT
+	if Input.is_action_pressed("mou esquerra"):
+		direccio = Vector2.LEFT
+	if Input.is_action_pressed("mou amunt"):
+		direccio = Vector2.UP
+	if Input.is_action_pressed("mou avall"):
+		direccio = Vector2.DOWN
+	if Input.is_action_pressed("gira"):
+		rotation_degrees += 10
+	if Input.is_action_pressed("augmenta velocitat"):
+		velocitat += 1
+	if Input.is_action_pressed("disminueix velocitat"):
+		velocitat -= 1
+	if Input.is_action_pressed("augmenta mida"):
+		scale += Vector2(0.1,0.1)
+	if Input.is_action_pressed("disminueix mida"):
+		scale += Vector2(-0.1,-0.1)
+	position += velocitat * delta *direccio.normalized()
+	direccio = Vector2(0,0)
+
+
+func _on_Personatge_area_entered(area):
+	modulate = Color(1,0,0)
+
+
+func _on_Personatge_area_exited(area):
+	modulate = Color(1,1,1)
